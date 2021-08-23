3f230108f5142c0c63f0fe82565b4609f0029908
Viswa1999 <85360991+Viswa1999@users.noreply.github.com>
2021-08-23 14:17:40 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Products
-
+#this is view file for trees file
 def ProductPage(request):
     products = Products.objects.all()
     return render(request,'treeproducts.html',{'products':products})
